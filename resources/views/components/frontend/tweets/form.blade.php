781bfcee2c9b74c83cad4b740974cc5dd5195d7c
Hama Al Wahbi <hamadalwahbi@gmail.com>
2021-01-13 02:28:20 +0300

    Update form.blade.php

--- resources/views/components/frontend/tweets/form.blade.php
+++ resources/views/components/frontend/tweets/form.blade.php
@@ -3,7 +3,7 @@
     <form method="POST" action="{{ route('frontend.tweet.create') }}" enctype="multipart/form-data">
         @csrf
         <textarea name="content" placeholder="your text here" class="uk-textarea" rows="4"></textarea>
-        <button class="uk-button uk-button-primary uk-margin uk-width-1-1">つぶやく</button>
+        <button class="uk-button uk-button-primary uk-margin uk-width-1-1">Send Tweetく</button>
         <div class="js-upload uk-placeholder uk-text-center">
             <span uk-icon="icon: cloud-upload"></span>
             <span class="uk-text-middle">drop picture here or</span>
