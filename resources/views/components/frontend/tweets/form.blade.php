bb0d8d1fedc5056dd545626cefdb8e6c8335d67a
Hama Al Wahbi <hamadalwahbi@gmail.com>
2021-01-13 02:27:40 +0300

    Update form.blade.php

--- resources/views/components/frontend/tweets/form.blade.php
+++ resources/views/components/frontend/tweets/form.blade.php
@@ -2,7 +2,7 @@
     <x-partial.error-messages :errors="$errors"/>
     <form method="POST" action="{{ route('frontend.tweet.create') }}" enctype="multipart/form-data">
         @csrf
-        <textarea name="content" placeholder="つぶやいてみましょう" class="uk-textarea" rows="4"></textarea>
+        <textarea name="content" placeholder="your text here" class="uk-textarea" rows="4"></textarea>
         <button class="uk-button uk-button-primary uk-margin uk-width-1-1">つぶやく</button>
         <div class="js-upload uk-placeholder uk-text-center">
             <span uk-icon="icon: cloud-upload"></span>
