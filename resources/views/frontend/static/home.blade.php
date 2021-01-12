7ac5db287680a60fc091e00282e2313e8207c443
Hama Al Wahbi <hamadalwahbi@gmail.com>
2021-01-13 02:34:33 +0300

    Update home.blade.php

--- resources/views/frontend/static/home.blade.php
+++ resources/views/frontend/static/home.blade.php
@@ -6,10 +6,10 @@
 
 @section('content')
 <div class="uk-section uk-section-large uk-section-muted">
-    <h1 class="text-center">Welcome to Laravel tweet</h1>
+    <h1 class="text-center">Welcome to Sure tweets</h1>
     <hr class="my-4">
     <p class="text-center">
-        Laravelでtwitter風アプリケーションをドメイン駆動設計で開発するプロジェクト
+        
     </p>
     <a class="text-decoration-none" href="{{ route('frontend.auth.signup') }}">
         <button class="btn btn-outline-primary px-4 py-2 mx-auto d-block">Sign Up here</button>
